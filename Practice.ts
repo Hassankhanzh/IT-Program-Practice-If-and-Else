a991154fb37773ea7cc4a4a9c001c369ade32da0
Hassankhanzh <160410531+Hassankhanzh@users.noreply.github.com>
2024-03-12 14:43:59 +0500

    Add files via upload

---
+++ Practice.ts
@@ -0,0 +1,95 @@
+//IF & Else
+
+// Program # 1
+
+let Username: String = "Danish"
+let Pw: String = "000"
+let code: String = "0000"
+let Facecode: String = "12"
+
+if(Username === "Hassan" && Pw === "1234"){
+    console.log("Kindly provide code")
+    if(code === "4567"){
+        console.log("Welcome Home" + " " + Username)
+        if(Facecode === "11"){
+            console.log("Face Matched Open The Door")
+        
+        }
+    }
+}
+
+
+else if(Username === "Danish" && Pw === "000"){
+    console.log("Provide Code")
+    if(code === "0000"){
+        console.log("Welcome Home" + " " + Username)
+        if(Facecode === "12"){
+            console.log(Username + " " + "Face Matched Open The Door")
+            
+        }}}
+
+        else { console.log("Invalid Person")}
+
+
+//Program # 2
+
+let SName: String = "MK"
+let FName: String = "MSK"
+let Slot: String = "Tuesday"
+let Time: Number =  8
+
+
+
+if(SName === "MK" && FName === "MSK"){
+    console.log("Welcome")
+    if(Slot === "Tuesday" && Time === 8){
+        console.log("You are Right Time to Come Welcome" + SName)
+    }
+}
+else if(SName === "MK" && FName === "MSK"){
+    console.log("Welcome")
+    if(Slot === "Tuesday" && Time === 8){
+        console.log("You are Right Time to Come Welcome" + SName)
+    }
+}
+
+else{ console.log("Failed to Login")}
+
+// Program 3 (Result)
+
+let STName : String = "Hassan"
+let Result : String = "Pass"
+let Per : String = "B90"
+let Above90: String = "A1 Grade"
+let Below90: String = "A Grade"
+let Below80: String = "B Grade"
+let Below70: String = "Fail" 
+
+if(STName == STName && Result == "Pass" && Per == "A90"){ 
+    console.log("Weldone" + " " + "Mr / Ms." + STName + " " + "You Scored" + " " + Above90)
+
+}
+
+else if(STName == STName && Result == "Pass" && Per == "B90"){ 
+    console.log("Weldone" + " " + "Mr / Ms." + STName + " " + "You Scored" + " " + Below90)
+}
+
+else if(STName == STName && Result == "Pass" && Per == "B80"){ 
+    console.log("Weldone" + " " + "Mr / Ms." + STName + " " + "You Scored" + " " + Below80)}
+
+else{console.log("Failed")}
+
+//Program # 4 (For Loop)
+    
+let Hassan: string [] = ["Imran", "Sohail", "Kashif"]
+for(let i=2; i<Hassan.length; i++){
+    console.log(Hassan[i])
+}
+
+//Program # 5 (Table for Loop)
+
+let n1 = 10
+for(let h=1; h<=25; h++){
+    console.log(`${n1}*${h}=${n1*h}`)
+}
+
